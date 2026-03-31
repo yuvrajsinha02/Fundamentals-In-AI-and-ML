29c1d780aa7a54375f45246fd4271cef85656dc7
unethical-strange <yuvraj.25bce11305@vitbhopal.ac.in>
2026-03-31 20:44:05 +0530

    Adding language list

--- Translator.py
+++ Translator.py
@@ -3,3 +3,48 @@ import speech_recognition as sr
 from translate import Translator
 from gtts import gTTS
 import os
+
+flag = 0
+
+dic = ('afrikaans', 'af', 'albanian', 'sq',
+       'amharic', 'am', 'arabic', 'ar',
+       'armenian', 'hy', 'Assamese', 'as', 'azerbaijani', 'az',
+       'basque', 'eu', 'belarusian', 'be', 'bengali', 'bn',
+       'bosnian', 'bs', 'bulgarian', 'bg', 'burmese', 'my',
+       'catalan', 'ca', 'cebuano', 'ceb', 'chichewa', 'ny',
+       'chinese', 'zh', 'corsican', 'co', 'croatian', 'hr',
+       'czech', 'cs', 'danish', 'da', 'dutch', 'nl',
+       'english', 'en', 'esperanto', 'eo',
+       'estonian', 'et', 'filipino', 'tl', 'finnish',
+       'fi', 'french', 'fr', 'frisian', 'fy', 'galician',
+       'gl', 'georgian', 'ka', 'german', 'de',
+       'greek', 'el', 'gujarati', 'gu',
+       'haitian creole', 'ht', 'hausa', 'ha',
+       'hawaiian', 'haw', 'hebrew', 'he', 'hindi',
+       'hi', 'hmong', 'hmn', 'hungarian', 'hu',
+       'icelandic', 'is', 'igbo', 'ig', 'indonesian', 'id', 'irish', 'ga'
+       'italian', 'it', 'japanese', 'ja', 'javanese', 'jw',
+       'kannada', 'kn', 'kazakh', 'kk', 'khmer',
+       'km', 'korean', 'ko', 'kurdish (kurmanji)',
+       'ku', 'kyrgyz', 'ky', 'lao', 'lo',
+       'latin', 'la', 'latvian', 'lv', 'lithuanian',
+       'lt', 'luxembourgish', 'lb',
+       'macedonian', 'mk', 'malagasy', 'mg', 'malay',
+       'ms', 'malayalam', 'ml', 'maltese',
+       'mt', 'maori', 'mi', 'marathi', 'mr', 'mongolian',
+       'mn', 'myanmar', 'my',
+       'nepali', 'ne', 'norwegian', 'no', 'odia', 'or',
+       'pashto', 'ps', 'persian', 'fa',
+       'polish', 'pl', 'portuguese', 'pt', 'punjabi',
+       'pa', 'romanian', 'ro', 'russian',
+       'ru', 'samoan', 'sm', 'scots gaelic', 'gd',
+       'serbian', 'sr', 'sesotho', 'st',
+       'shona', 'sn', 'sindhi', 'sd', 'sinhala', 'si',
+       'slovak', 'sk', 'slovenian', 'sl',
+       'somali', 'so', 'spanish', 'es', 'sundanese',
+       'su', 'swahili', 'sw', 'swedish', 'sv',
+       'tajik', 'tg', 'tamil', 'ta', 'telugu', 'tibetan', 'bo',
+       'te', 'thai', 'th', 'turkish',
+       'tr', 'ukrainian', 'uk', 'urdu', 'ur', 'uyghur',
+       'ug', 'uzbek', 'uz', 'vietnamese', 'vi', 'welsh', 'cy', 'xhosa', 'xh',
+       'yiddish', 'yi', 'yoruba', 'yo', 'zulu', 'zu')
